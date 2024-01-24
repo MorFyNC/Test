f4ebc3e679ceb8eba9d37028c77d7444bee838b0
212223 <bulat.hisambeev@gmail.com>
2024-01-24 11:01:49 +0300

    Добавьте файлы проекта.

---
+++ Test/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalitySpace
+{
+    public static class Functionality
+    {
+        private static Stack<char> stack = new Stack<char>();
+        private static Dictionary<char, char> chars = new Dictionary<char, char>
+        {
+            {'(', ')' },
+            {'[', ']' },
+            {'{', '}' }
+        };
+
+        public static bool CheckOrder(string input)
+        {
+            foreach (char c in input)
+            {
+                if (chars.ContainsKey(c))
+                {
+                    stack.Push(c);
+                }
+                else if (chars.ContainsValue(c))
+                {
+                    if (stack.Count == 0 || chars[stack.Pop()] != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using FunctionalitySpace;
+
+string input = Console.ReadLine();
+string output = "";
+
+foreach (char s in input)
+{
+    if (s == '(' || s == ')' || s == '[' || s == ']' || s == '{' || s == '}')
+    {
+        output += s;
+    }
+
+}
+
+if (Functionality.CheckOrder(input))
+{
+    
+    Console.WriteLine($"Правильный порядок: {output}");
+} 
+else
+{
+    Console.WriteLine($"Неправильный порядок: {output}");
+}
